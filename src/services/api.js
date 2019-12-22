5e24d820e8ab6bfa3207d595f27f25a7d83997e2
Fernando Zuffo <fzuffo@gmail.com>
2019-12-22 00:02:50 -0400

    adjust api settings

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.REACT_APP_BASE_URL;
+// const BASE_URL = process.env.REACT_APP_BASE_URL;
+const BASE_URL = http://159.203.127.210;
+
 
 const api = axios.create({
   baseURL: BASE_URL,
